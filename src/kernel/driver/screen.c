c3d4d102530b8303b027253303815d84840d7836
Qcho <qcho86@gmail.com>
2011-05-18 01:08:59 -0300

    added constants & better backspace

--- src/kernel/driver/screen.c
+++ src/kernel/driver/screen.c
@@ -4,6 +4,12 @@
 int16_t *video_memory = (int16_t *)0xB8000;
 // Stores the cursor position.
 
+#define SCREEN_SIZE_X 80
+#define SCREEN_SIZE_Y 25
+
+#define BACKSPACE '\x08'
+#define ESC '\x1B'
+
 uint8_t cursor_x = 0;
 uint8_t cursor_y = 0;
 
@@ -26,8 +32,8 @@ void screen_setColours(enum Colour colourBack, enum Colour colourFore) {
 // Updates the hardware cursor.
 static void move_cursor()
 {
-    // The screen is 80 characters wide...
-    int16_t cursorLocation = cursor_y * 80 + cursor_x;
+    // The screen is SCREEN_SIZE_X characters wide...
+    int16_t cursorLocation = cursor_y * SCREEN_SIZE_X + cursor_x;
     outb(0x3D4, 14);                  // Tell the VGA board we are setting the high cursor byte.
     outb(0x3D5, cursorLocation >> 8); // Send the high cursor byte.
     outb(0x3D4, 15);                  // Tell the VGA board we are setting the low cursor byte.
@@ -42,25 +48,25 @@ static void scroll()
     uint8_t attributeByte = (0 /*black*/ << 4) | (15 /*white*/ & 0x0F);
     int16_t blank = 0x20 /* space */ | (attributeByte << 8);
 
-    // Row 25 is the end, this means we need to scroll up
-    if(cursor_y >= 25)
+    // Row SCREEN_SIZE_Y is the end, this means we need to scroll up
+    if(cursor_y >= SCREEN_SIZE_Y)
     {
         // Move the current text chunk that makes up the screen
         // back in the buffer by a line
         int i;
-        for (i = 0*80; i < 24*80; i++)
+        for (i = 0*SCREEN_SIZE_X; i < (SCREEN_SIZE_Y-1)*SCREEN_SIZE_X; i++)
         {
-            video_memory[i] = video_memory[i+80];
+            video_memory[i] = video_memory[i+SCREEN_SIZE_X];
         }
 
         // The last line should now be blank. Do this by writing
-        // 80 spaces to it.
-        for (i = 24*80; i < 25*80; i++)
+        // SCREEN_SIZE_X spaces to it.
+        for (i = (SCREEN_SIZE_Y-1)*SCREEN_SIZE_X; i < SCREEN_SIZE_Y*SCREEN_SIZE_X; i++)
         {
             video_memory[i] = blank;
         }
         // The cursor should now be on the last line.
-        cursor_y = 24;
+        cursor_y = (SCREEN_SIZE_Y-1);
     }
 }
 
@@ -77,14 +83,20 @@ void screen_put(char c)
     int16_t *location;
 
     // Handle a backspace, by moving the cursor back one space
-    if (c == 0x08 && cursor_x)
-    {
-        cursor_x--;
+    if (c == BACKSPACE) {
+        if(cursor_x) {
+            cursor_x--;
+        } else if (cursor_y) {
+            cursor_x=SCREEN_SIZE_X-1;
+            cursor_y--;
+        }
+        location = video_memory + (cursor_y*SCREEN_SIZE_X + cursor_x);
+        *location = ' ' | attribute;
     }
 
     // Handle a tab by increasing the cursor's X, but only to a point
     // where it is divisible by 8.
-    else if (c == 0x09)
+    else if (c == '\t')
     {
         cursor_x = (cursor_x+8) & ~(8-1);
     }
@@ -104,14 +116,14 @@ void screen_put(char c)
     // Handle any other printable character.
     else if(c >= ' ')
     {
-        location = video_memory + (cursor_y*80 + cursor_x);
+        location = video_memory + (cursor_y*SCREEN_SIZE_X + cursor_x);
         *location = c | attribute;
         cursor_x++;
     }
 
     // Check if we need to insert a new line because we have reached the end
     // of the screen.
-    if (cursor_x >= 80)
+    if (cursor_x >= SCREEN_SIZE_X)
     {
         cursor_x = 0;
         cursor_y ++;
@@ -132,7 +144,7 @@ void screen_clear()
     int16_t blank = 0x20 /* space */ | (attributeByte << 8);
 
     int i;
-    for (i = 0; i < 80*25; i++)
+    for (i = 0; i < SCREEN_SIZE_X*SCREEN_SIZE_Y; i++)
     {
         video_memory[i] = blank;
     }
