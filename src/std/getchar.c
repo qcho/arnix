6f26b1e56ae8dc8576474118e7a075ddbd548718
Axel Wassington <axelwass@gmail.com>
2011-05-23 15:46:57 -0300

    flushes when enter and stream size added

--- src/std/getchar.c
+++ src/std/getchar.c
@@ -1,22 +1,63 @@
 #include "stdio.h"
 #include "printf.c"
 
-char stream[100];
-char * streamp=stream;
+#define STREAM_SIZE 500
+
+typedef int (*flusher)(char * streampointer);
+
+char stream[STREAM_SIZE];
+char * streamout=stream;
+
+
+int introflush(char * streampointer){
+	for(;*streampointer!='\0';streampointer++){
+		if(*streampointer=='\n' || STREAM_SIZE-(streampointer-stream)-1){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void delete_stream_backspaces(){
+	int i, j;
+	for(i=0;stream[i]!='\0';i++){
+		if(stream[i]=='\b'){
+			for(j=i;stream[j]=='\b';j--);
+			stream[j]='\b';
+		}
+	}
+	for(i=0;stream[i]!='\0';i++){
+		if(stream[i]=='\b'){
+			for(j=i;stream[j]=='\b';j++);
+			stream[i]=stream[j];
+			stream[j]='\b';
+		}
+	}
+	i--;
+	for(;stream[i]=='\b';i--);
+	i++;
+	stream[i]='\0';
+
+}
 
 
 char getchar(){
-	char c=*streamp;
+	char c=*streamout;
 	if(c=='\0'){
-		streamp=stream;
+		streamout=stream;
+		char * streamin=stream;
 		stream[0]='\0';
-		while(stream[0]=='\0'){
-			__read(0,stream,100);
-			printf(stream);
+		while(!introflush(streamin)){
+			for(;*streamin!='\0';streamin++);
+			__read(0,streamin,STREAM_SIZE-(streamin-stream)-1);
+			printf(streamin);
 		}
-		c=*streamp;
+		//delete_stream_backspaces();
+		c=*streamout;
 	}
-	streamp++;
+	streamout++;
 	return c; 
 	
 }
+
+
