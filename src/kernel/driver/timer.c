196d6719dcb93092286aa36de0dd6a208686388a
tomas <tomas@tomas.(none)>
2011-05-29 13:02:16 -0300

    cpuspeed stuffs

--- src/kernel/driver/timer.c
+++ src/kernel/driver/timer.c
@@ -1,1 +1,27 @@
+#include "timer.h"
+#include "../system/isr.h"
+#include "../system/in_out.h"
+#include "ascii.h"
 
+
+
+int ticks;
+
+int getTicks(){
+  return ticks;
+}
+void startTicks(){
+  ticks = 0;
+}
+void stopTicks(){
+  ticks = 0;
+}
+
+
+void IRQ0_handler(registers_t reg){
+	ticks++;
+}
+
+void init_timer(){
+	register_interrupt_handler(IRQ0,IRQ0_handler);
+}
