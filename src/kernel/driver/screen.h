942444cd69f7688c6fe44f40012adf49a3a5a41a
Qcho <qcho86@gmail.com>
2011-05-30 03:23:17 -0300

    comment tidy

--- src/kernel/driver/screen.h
+++ src/kernel/driver/screen.h
@@ -19,25 +19,15 @@
  * 4	Underscore (on monochrome display adapter only)
  * 5	Blink on
  *  
- * Foreground colors
- * 30	Black
- * 31	Red
- * 32	Green
- * 33	Yellow
- * 34	Blue
- * 35	Magenta
- * 36	Cyan
- * 37	White
- *  
- * Background colors
- * 40	Black
- * 41	Red
- * 42	Green
- * 43	Yellow
- * 44	Blue
- * 45	Magenta
- * 46	Cyan
- * 47	White  
+ * Foreground colors    Background colors
+ * 30	Black           40	Black
+ * 31	Red             41	Red
+ * 32	Green           42	Green
+ * 33	Yellow          43	Yellow
+ * 34	Blue            44      Blue
+ * 35	Magenta         45      Magenta
+ * 36	Cyan            46      Cyan
+ * 37	White           47      White 
  * 
  * Ej: Esc[34;47m (azul en fondo blanco)
  **/
