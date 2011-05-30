688ce3263a4ee648b58694e64d94341a4eb6e12c
Qcho <qcho86@gmail.com>
2011-05-30 01:46:50 -0300

    08 handler not being called anymore

--- src/libasm.asm
+++ src/libasm.asm
@@ -51,23 +51,6 @@ _lidt:				; Carga el IDTR
         pop     ebp
         retn
 
-
-_int_08_hand:				; Handler de INT 8 ( Timer tick)
-        push    ds
-        push    es                      ; Se salvan los registros
-        pusha                           ; Carga de DS y ES con el valor del selector
-        mov     ax, 10h			; a utilizar.
-        mov     ds, ax
-        mov     es, ax                  
-        call    int_08                 
-        mov	al,20h			; Envio de EOI generico al PIC
-	out	20h,al
-	popa                            
-        pop     es
-        pop     ds
-        iret
-
-
 ; Debug para el BOCHS, detiene la ejecució; Para continuar colocar en el BOCHSDBG: set $eax=0
 ;
 
