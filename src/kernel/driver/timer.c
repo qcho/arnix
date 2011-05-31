136ef21e66508cac161cc379ae1de2c7b15202b5
Qcho <qcho86@gmail.com>
2011-05-31 20:45:14 -0300

    scanf and test sequences

--- src/kernel/driver/timer.c
+++ src/kernel/driver/timer.c
@@ -8,44 +8,44 @@ int80_t sub_handler_vec[SUB_FUNC_VEC_SIZE];
 int ticks;
 int count_ticks;
 int sub_func_count;
+unsigned long k;
 
 void register_tick_subhandler(int80_t func) {
-	if(sub_func_count<SUB_FUNC_VEC_SIZE-1){
-	    	sub_handler_vec[sub_func_count] = func;
-		sub_func_count++;
-	}
+    if (sub_func_count < SUB_FUNC_VEC_SIZE - 1) {
+        sub_handler_vec[sub_func_count] = func;
+        sub_func_count++;
+    }
 }
 
-
-void IRQ0_handler(registers_t regs){
-	int i;
-	if(count_ticks){
-		ticks++;
-	}
-	for(i=0;i<sub_func_count;i++){
-		sub_handler_vec[i](regs);
-	}
+void IRQ0_handler(registers_t regs) {
+    int i;
+    if (count_ticks) {
+        if (ticks == 0) {
+            k = getRDTSC();
+        }
+        ticks++;
+    }
+    for (i = 0; i < sub_func_count; i++) {
+        sub_handler_vec[i](regs);
+    }
 }
 
-void cpu_speed(registers_t regs){
-	unsigned long k;
-	count_ticks=1;
-	ticks=0;
-        int TICK_COUNT = 30;
-	_Sti();
-	k=getRDTSC();
-	while(ticks<TICK_COUNT);
-	k=getRDTSC()-k;
-	_Cli();
-	count_ticks=0;
-	*((unsigned long*)regs.ebx)=(k/TICK_COUNT)*18+k/(TICK_COUNT*5);
+void cpu_speed(registers_t regs) {
+    count_ticks = 1;
+    ticks = -1;
+    _Sti();
+    while (ticks < 30);
+    k = getRDTSC() - k;
+    _Cli();
+    count_ticks = 0;
+    *((unsigned long*) regs.ebx) = (k / ticks)*18 + k / (ticks * 5);
 }
 
-void init_timer_tick(){
-	sub_func_count=0;
-	count_ticks=0;
-	register_interrupt_handler(IRQ0,IRQ0_handler);
-	register_functionality(5,cpu_speed);
+void init_timer_tick() {
+    sub_func_count = 0;
+    count_ticks = 0;
+    register_interrupt_handler(IRQ0, IRQ0_handler);
+    register_functionality(5, cpu_speed);
 }
 
 
